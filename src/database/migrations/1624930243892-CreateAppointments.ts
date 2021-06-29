add25968a1c6e209f7011df6a243236ab5c5366d
LuizTimboAlcantara <60242867+LuizTimboAlcantara@users.noreply.github.com>
2021-06-29 10:02:40 -0300

    Relacionamentos entre os models Appointments e Users.

--- src/database/migrations/1624930243892-CreateAppointments.ts
+++ src/database/migrations/1624930243892-CreateAppointments.ts
@@ -8,7 +8,7 @@ export default class CreateAppointments1624930243892 implements MigrationInterfa
         columns: [
           {
             name: "id",
-            type: "varchar",
+            type: "uuid",
             isPrimary: true,
             generationStrategy: "uuid",
             default: "uuid_generate_v4()",
--- src/database/migrations/1624969460131-CreateUsers.ts
+++ src/database/migrations/1624969460131-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1624969460131 implements MigrationInterface {
         columns: [
           {
             name: "id",
-            type: "varchar",
+            type: "uuid",
             isPrimary: true,
             generationStrategy: "uuid",
             default: "uuid_generate_v4()",
