3af59f39e9a5ed7c78cf5c8304ea1749d632009e
LuizTimboAlcantara <60242867+LuizTimboAlcantara@users.noreply.github.com>
2021-06-30 11:59:14 -0300

    Atualizando avatar.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -5,6 +5,7 @@ import uploadConfig from "../config/upload";
 import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 
 import CreateUserService from "../services/CreateUserService";
+import UpdateAvatarUserService from "../services/UpdateAvatarUserService";
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -23,8 +24,24 @@ usersRouter.post("/", async (request, response) => {
   }
 });
 
-usersRouter.patch("/avatar", ensureAuthenticated, upload.single("avatar"), async (request, response) => {
-  return response.json("ok");
-});
+usersRouter.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  async (request, response) => {
+    try {
+      const updateAvatarUserService = new UpdateAvatarUserService();
+
+      const user = await updateAvatarUserService.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  }
+);
 
 export default usersRouter;
