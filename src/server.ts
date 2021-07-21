593f1ae03cbf14d11ef670e9e490556b49bfcfd5
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-07-20 21:22:09 -0300

    Configurando Cors.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
 import "express-async-errors";
 import "reflect-metadata";
 
@@ -12,6 +13,7 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(uploadConfig.directory));
 app.use(routes);
