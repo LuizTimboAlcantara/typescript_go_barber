cef2604b138ec034d63bea46593510a52a8e41cd
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-07-27 20:44:35 -0300

    Inversão de dependência em appointment.

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,29 +1,31 @@
 import { startOfHour } from "date-fns";
-import { getCustomRepository } from "typeorm";
 
 import AppError from "@shared/errors/AppError";
 
 import Appointment from "../infra/typeorm/entities/Appointment";
-import AppointmentsRepository from "../repositories/AppointmentsRepository";
+import IAppointmentsRepository from "../repositories/IAppointmentsRepository";
 
-interface Request {
+interface IRequest {
   provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ provider_id, date }: Request): Promise<Appointment> {
-    const appointmentRepository = getCustomRepository(AppointmentsRepository);
+  constructor(private appointmentsRepository: IAppointmentsRepository) {}
 
+  public async execute({ provider_id, date }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentRepository.findByDate(appointmentDate);
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate);
 
     if (findAppointmentInSameDate) {
       throw new AppError("This appointment is already booked");
     }
 
-    const appointment = await appointmentRepository.create({ provider_id, date: appointmentDate });
+    const appointment = await this.appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
 
     return appointment;
   }
